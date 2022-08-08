04bffcbd0768e1359ae8422095e4d2cad9d73d75
stalkerist7474 <80361659+stalkerist7474@users.noreply.github.com>
2022-08-08 19:53:30 +0300

    1

--- electricity/station/models.py
+++ electricity/station/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 
@@ -7,10 +8,27 @@ class Station(models.Model):
 
     date_created = models.DateField(auto_now_add = True)
     detailed_time_created = models.TimeField(auto_now_add = True)
+
+    class Meta:
+        
+        verbose_name = 'Подстанция'
+        verbose_name_plural = 'Подстанции'
+
+    def __str__(self):
+        return self.description
+
     
 
 class Worker(models.Model):
-    Name = models.CharField(max_length = 255)
+    name = models.CharField(max_length = 255)
     position = models.CharField(max_length = 255,null=True)
+
+    class Meta:
+        
+        verbose_name = 'Работник'
+        verbose_name_plural = 'Работники'
+
+    def __str__(self):
+        return self.Name
     
 
